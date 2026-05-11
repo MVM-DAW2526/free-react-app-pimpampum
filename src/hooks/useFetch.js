e681ee704a1c65b7a3d8729188ed08e984f0878d
marija <mmilicevic@institutmvm.cat>
2026-05-11 15:40:05 +0200

    Added Fetch url

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [carregant, setCarregant] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json.docs);
+      } catch (err) {
+        setError('Alguna cosa ha anat malament en la cerca de llibres');
+      } finally {
+        setCarregant(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, carregant, error };
+}
