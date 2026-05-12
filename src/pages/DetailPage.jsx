1b12daa3c4c655dcd9326225c3c6622b0e80cdd9
Rucla16 <lopritescu@institutmvm.cat>
2026-05-12 21:29:42 +0200

    Retocar index.css

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,19 +1,22 @@
 export default function DetailPage({ book, goBack }) {
   return (
-    <div>
+    <div id="bookInfo">
       <button onClick={goBack}>Tornar enrere</button>
 
       {book.cover_i && (
         <img
+        id={"bookCover"}
         src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`}
         alt={book.title}
         />
       )}
 
-      <h2>{book.title}</h2>
-      <p><strong>Autor:</strong> {book.author_name?.[0] ?? 'Desconegut'}</p>
-      <p><strong>Any:</strong> {book.first_publish_year ?? 'Desconegut'}</p>
-      <p><strong>Edicions:</strong> {book.edition_count ?? 'Desconegut'}</p>
+      <div id="bookData">
+        <h2>{book.title}</h2>
+        <p><strong>Autor:</strong> {book.author_name?.[0] ?? 'Desconegut'}</p>
+        <p><strong>Any:</strong> {book.first_publish_year ?? 'Desconegut'}</p>
+        <p><strong>Edicions:</strong> {book.edition_count ?? 'Desconegut'}</p>
+      </div>
     </div> 
   )
 }
